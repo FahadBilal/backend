7152d870dd6a712d6faf91c926d0a16d05d61dab
Fahad Bilal <fahadbilalfmni3321@gmail.com>
2024-10-28 18:58:35 +0500

    set app & error response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
-
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
-    path:"./.env"
-})
+  path: "./.env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is listening at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed", err);
+  });
