df7e847d8e3ebd742a511860a05a27a18cd6ab4a
Fahad Bilal <fahadbilalfmni3321@gmail.com>
2024-10-31 18:38:06 +0500

    add auth middleware and logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -89,7 +89,9 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new ApiError(401, "Invalid user crenditials");
   }
 
-  generateAccessAndRefreshToken(user._id);
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
 
   const loggedIn = await User.findById(user._id).select(
     "-password -refreshToken"
@@ -109,10 +111,36 @@ const loginUser = asyncHandler(async (req, res) => {
         200,
         {
           user: loggedIn,
+          accessToken,
+          refreshToken,
         },
         "User logged In Successfully"
       )
     );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: "",
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    http: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logout Successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,17 @@
-import {Router} from 'express'
-import { loginUser, registerUser } from '../controllers/user.controller.js';
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerUser);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-
-
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
